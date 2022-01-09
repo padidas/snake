31892fd6e7d2f827375bccd1519e713ff0c0d302
Patrick Baber <paddybaber@gmail.com>
2022-01-09 17:56:11 +0100

    styling

--- src/components/RestartButton.svelte
+++ src/components/RestartButton.svelte
@@ -4,6 +4,6 @@
 
 <button
 	on:click={restart}
-	class="w-[100px] h-[50px] rounded-full bg-red-600 font-bold border-2 border-white absolute left-[35%] top-[39%] flex justify-center items-center z-10"
+	class="w-[100px] h-[20px] rounded-full bg-red-600 font-bold border-2 border-white flex justify-center items-center z-10"
 	>RESTART</button
 >
--- src/components/ScoreSection.svelte
+++ src/components/ScoreSection.svelte
@@ -14,21 +14,22 @@
 </script>
 
 <input
-	class="bg-transparent h-16 border-4 text-center rounded-xl border-blue-300 w-[140px] font-extrabold text-2xl"
+	class="bg-transparent h-14 border-4 text-center rounded-xl border-blue-300 w-[140px] font-extrabold text-lg"
+	placeholder="Dein Name..."
 	bind:value={username}
 />
 <div
-	class="border-4 rounded-xl border-blue-300 w-20 h-16 flex justify-center items-center font-extrabold text-2xl"
+	class="border-4 rounded-xl border-blue-300 w-20 h-14 flex justify-center items-center font-extrabold text-2xl"
 >
 	{score}
 </div>
 <div
-	class="flex flex-col justify-center items-center w-20 h-16 rounded-xl border-4 border-yellow-400"
+	class="flex flex-col justify-center items-center w-20 h-14 rounded-xl border-4 border-yellow-400"
 >
 	<div class="font-semibold">
 		{highscore.username}
 	</div>
-	<div class="font-extrabold text-2xl">
+	<div class="font-extrabold">
 		{highscore.score}
 	</div>
 </div>

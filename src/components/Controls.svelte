97090b8489e6708447a5f2f9f12e5b68a7732155
Patrick Baber <paddybaber@gmail.com>
2022-01-29 23:43:21 +0100

    controls as button (not div)

--- src/components/Controls.svelte
+++ src/components/Controls.svelte
@@ -3,9 +3,9 @@
 	export let label: string
 </script>
 
-<div
+<button
 	on:click={controlFunction}
-	class="flex justify-center uppercase items-center dark-gradient rounded-full border-2 border-white w-40"
+	class="flex justify-center uppercase items-center dark-gradient rounded-md border-2 border-white w-40"
 >
 	{label}
-</div>
+</button>

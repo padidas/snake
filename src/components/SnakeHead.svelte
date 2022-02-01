3b10c8c45a8e76ceb8fc4d5a1061aa03030b8d08
Patrick Baber <paddybaber@gmail.com>
2022-02-02 00:10:41 +0100

    snake face

--- src/components/SnakeHead.svelte
+++ src/components/SnakeHead.svelte
@@ -7,10 +7,10 @@
 
 <div class="relative z-20" style="transform:rotate({headRotation})">
 	<div class="w-[24px] h-[24px] snake-color rounded-t-full" class:growing />
-	<div class="absolute w-[5px] h-[5px] rounded-full bg-gray-900 top-[12px] left-[3px]" />
-	<div class="absolute w-[5px] h-[5px] rounded-full bg-gray-900 top-[12px] right-[3px]" />
-	<div class="absolute w-[2px] h-[2px] rounded-full bg-gray-900 top-[5px] left-[5px]" />
-	<div class="absolute w-[2px] h-[2px] rounded-full bg-gray-900 top-[5px] right-[5px]" />
-	<div class="absolute w-[2px] h-[2px] rounded-full bg-gray-200 top-[13px] left-[5px]" />
-	<div class="absolute w-[2px] h-[2px] rounded-full bg-gray-200 top-[13px] right-[5px]" />
+	<div class="absolute w-[5px] h-[5px] rounded-full bg-gray-900 top-[11px] left-[3px]" />
+	<div class="absolute w-[5px] h-[5px] rounded-full bg-gray-900 top-[11px] right-[3px]" />
+	<div class="absolute w-[2px] h-[1px] rounded bg-gray-900 top-[5px] left-[6px] -rotate-45" />
+	<div class="absolute w-[2px] h-[1px] rounded bg-gray-900 top-[5px] right-[6px] rotate-45" />
+	<div class="absolute w-[2px] h-[2px] rounded-full bg-gray-200 top-[12px] left-[5px]" />
+	<div class="absolute w-[2px] h-[2px] rounded-full bg-gray-200 top-[12px] right-[5px]" />
 </div>

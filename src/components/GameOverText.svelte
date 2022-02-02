3856a5d29d9033289c04b1688bf51da0bdd754a6
Patrick Baber <paddybaber@gmail.com>
2022-02-02 22:39:33 +0100

    game over screen transparent and faster

--- src/components/GameOverText.svelte
+++ src/components/GameOverText.svelte
@@ -4,17 +4,17 @@
 
 <div
 	out:fade={{ duration: 400 }}
-	class="flex flex-col justify-center rounded font-serif items-center bg-black w-[336px] absolute top-0 bottom-0 m-auto left-0 right-0 z-10"
+	class="flex flex-col justify-center rounded font-serif items-center bg-black bg-opacity-70 w-[336px] absolute top-0 bottom-0 m-auto left-0 right-0 z-10"
 >
 	<p
-		in:fade={{ delay: 400, duration: 1400 }}
+		in:fade={{ delay: 100, duration: 1000 }}
 		out:fade={{ duration: 400 }}
 		class="text-red-700 font-extrabold text-3xl"
 	>
 		YOU DIED
 	</p>
 	<p
-		in:fade={{ delay: 1400, duration: 1000 }}
+		in:fade={{ delay: 500, duration: 1000 }}
 		out:fade={{ duration: 400 }}
 		class="text-red-700 font-extrabold text"
 	>

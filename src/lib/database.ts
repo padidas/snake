f1c23418e86a08ede8ade6807844ca0c6e9a7f52
padidas <paddybaber@gmail.com>
2023-02-04 12:06:08 +0100

    wip: aggregation

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,10 +1,66 @@
 import { PrismaClient, type score } from '@prisma/client'
+import type { Score } from 'src/model/Types'
 export const prisma = new PrismaClient()
 
-export async function getTop10Players(): Promise<score[]> {
+export async function getTop10Players(): Promise<Score[]> {
 	await prisma.$connect()
 
-	return await prisma.score.findMany({ take: 10 })
+	const x = await prisma.score.groupBy({
+		by: ['normalizedUsername'],
+		where: {
+			score: {
+				gte: 150,
+			},
+		},
+		_max: {
+			score: true,
+		},
+		orderBy: {
+			_max: {
+				score: 'desc',
+			},
+		},
+		take: 10,
+	})
+
+	const djd = x.map((elem, i) => {
+		const a: Score = {
+			privateMode: false,
+			score: elem._max.score,
+			scoreId: '' + i,
+			snakeLength: 2,
+			username: elem.normalizedUsername,
+		}
+
+		return a
+	})
+
+	console.log(djd)
+	return djd
+
+	// return await prisma.score.findMany({ take: 10 })
+
+	// const myScores = await prisma.score.aggregate({
+	// 	where: {
+	// 		normalizedUsername: {
+	// 			equals: 'padidas',
+	// 		},
+	// 	},
+	// 	orderBy: {
+	// 		score: 'desc',
+	// 	},
+	// 	take: 10,
+	// })
+
+	// const scroy = myScores.map(score => ({
+	// 	score: score.score,
+	// 	privateMode: score.privateMode,
+	// 	username: score.username,
+	// 	scoreId: score.id,
+	// 	snakeLength: score.snakeLength,
+	// }))
+
+	// return scroy
 }
 
 // val aggregation = newAggregation(
